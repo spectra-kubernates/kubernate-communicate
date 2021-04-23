183b51a677949d1fcddddcbad5f0ead5d82c9191
unknown <aniket.harode@gmail.com>
2021-04-23 20:58:36 +0530

    added the context root

--- src/main/java/com/service/proxy/CommunicationController.java
+++ src/main/java/com/service/proxy/CommunicationController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("/communication")
+@RequestMapping("/communication")
 public class CommunicationController {
 	
 	@Autowired
